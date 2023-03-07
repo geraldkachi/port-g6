1398824f8d8f2de56d74e424c57578d689b63864
geraldkachi <fitzgeraldkachi@gmail.com>
2023-03-07 09:04:55 +0100

    portfolio

---
+++ src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { MailchimpForm } from "./MailchimpForm";
+import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+import { Link } from "react-router-dom";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <MailchimpForm />
+          <Col size={12} sm={6}>
+            <img src={logo} alt="Logo" />
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/fitzgerald-kachi/"><img src={navIcon1} alt="icon1" /></a>
+              <a href="https://facebook.com/Adimora.kachifizgerald"><img src={navIcon2} alt="icon2" /></a>
+              <a href="https://instagram.com/geraldkachi"><img src={navIcon3} alt="icon3" /></a>
+            </div>
+            <p>Copyright 2022. All Rights Reserved</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
